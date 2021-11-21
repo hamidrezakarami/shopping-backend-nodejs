a3b8125bbbcf89e4b2f657aab25f4f0e367a7965
hamidrezakarami <hamidrezakarami16@gmail.com>
2021-11-21 10:28:28 +0330

    [fix] delete extra lines

--- app/controllers/goods.controller.js
+++ app/controllers/goods.controller.js
@@ -6,8 +6,6 @@ exports.getGoods = asyncHandler(async (req, res, next) => {
   await client.query(
     `SELECT * FROM goods`,
     (err, result) => {
-      console.log('bala');
-      console.log(result);
       const { rows } = result;
       if (!err) res.status(200).json(rows);
     }
@@ -20,9 +18,7 @@ exports.getSingleGood = asyncHandler(async (req, res, next) => {
     `SELECT * FROM goods g WHERE g.goodid = $1`,
     [id],
     (err, result) => {
-      if(result){
-        console.log('paiin');
-        console.log(result);
+      if (result) {
         const { rows } = result;
         if (!err) res.status(200).json(rows);
       }
@@ -31,26 +27,32 @@ exports.getSingleGood = asyncHandler(async (req, res, next) => {
 });
 
 exports.createGood = asyncHandler(async (req, res, next) => {
-  const { firstName, lastName, dateOfBirth, phoneNumber, classID } = req.body;
+  const { detail, name, amount, price } = req.body;
   await client.query(
-    `CALL public.insert_student($1,$2,$3,$4,$5)`,
-    [firstName, lastName, dateOfBirth, phoneNumber, classID],
+    `INSERT INTO goods (detail, name, amount, price)
+    VALUES($1,$2,$3,$4)`,
+    [detail, name, amount, price],
     (err, result) => {
       if (!err) res.status(200).json({ Data: [], Success: true, Message: "" });
-      else res.status(500).json({ Data: [], Success: false, Message: "" });
+      else {
+        res.status(500).json({ Data: [], Success: false, Message: "" });
+      }
     }
   );
 });
 
-// @desc        Update student information
-// @route       PUT /api/student
-// @access      Public
 exports.updateGood = asyncHandler(async (req, res, next) => {
-  const { sid, firstName, lastName, dateOfBirth, phoneNumber, classID } =
+  const { detail, name, amount, price, goodid } =
     req.body;
   await client.query(
-    `CALL public.update_student($1,$2,$3,$4,$5,$6)`,
-    [sid, firstName, lastName, dateOfBirth, phoneNumber, classID],
+    `UPDATE goods
+    SET 
+    detail= $1,
+    name= $2,
+    amount= $3,
+    price= $4
+    WHERE goodid = $5;`,
+    [detail, name, amount, price, goodid],
     (err, result) => {
       if (!err) res.status(200).json({ Data: [], Success: true, Message: "" });
       else res.status(500).json({ Data: [], Success: false, Message: "" });
@@ -58,12 +60,9 @@ exports.updateGood = asyncHandler(async (req, res, next) => {
   );
 });
 
-// @desc        Delete a student
-// @route       Delete /api/student/:id
-// @access      Public
 exports.deleteGood = asyncHandler(async (req, res, next) => {
   const { id } = req.params;
-  await client.query(`CALL public.delete_student($1);`, [id], (err, result) => {
+  await client.query(`DELETE FROM goods WHERE goodid = $1`, [id], (err, result) => {
     if (!err) res.status(200).json({ Data: [], Success: true, Message: "" });
     else res.status(500).json({ Data: [], Success: false, Message: "" });
   });
